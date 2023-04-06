18cd4d0c88a21176ed21cafa00bb9114023ca85c
Sai Pavan Nelavalli <sai.n@media.net>
2023-04-06 15:25:44 +0530

    Log query and headers

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express()
 app.get("/", function (req, res) {
 
   console.log('#######################');
-  console.log(JSON.stringify(req.headers, null, 2));
+  console.log(JSON.stringify({query: params.query, headers: req.headers}, null, 2));
   console.log('#######################');
   
   res.send(`
